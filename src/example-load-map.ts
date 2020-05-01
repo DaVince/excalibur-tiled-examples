6ff1af1307b319fbe75b161fc742a76dd1240eaf
Vincent Beers <vincent.beers@sogeti.com>
2020-05-01 15:51:13 +0200

    Add basic map loading example

---
+++ src/example-load-map.ts
@@ -0,0 +1,14 @@
+import { TiledResource } from "@excaliburjs/excalibur-tiled";
+import { game } from './index';
+import { Loader } from 'excalibur';
+
+// This is a basic example showcasing how to load and display a map.
+
+const map = new TiledResource("/static/map.json"); // Prepare map resource
+const loader = new Loader([map]); // Prepare loader, queue up the map resource
+
+game.start(loader) // Load the resources given to the loader
+  .then(() => {    // Once they are loaded...
+  const tm = map.getTileMap(); // Get the generated Excalibur TileMap
+  game.add(tm);    // Add the TileMap to the default root scene.
+});
