4ec98e57dcd217193774163bb39c7f436e1f0548
Vincent Beers <vincent.beers@sogeti.com>
2020-09-18 14:59:43 +0200

    Improve code comments

--- src/example-external-tileset.ts
+++ src/example-external-tileset.ts
@@ -3,6 +3,10 @@ import { game } from './index';
 import { Loader } from 'excalibur';
 
 // A map with an external tileset is loaded.
+// map-external-tileset.json refers to tilemap.json for its actual tilemap data.
+
+// This is unlike map.json, which has the tilemap embedded. When you have many
+// maps, this will bloat your map file sizes with lots of redundant data.
 
 export function ExternalTilesetExample() {
   const map = new TiledResource("/static/map-external-tileset.json");
--- src/example-load-map.ts
+++ src/example-load-map.ts
@@ -10,13 +10,16 @@ export function BasicMapExample() {
   const loader = new Loader([map, texture]); // Prepare loader, queue up the map resource
   
   game.start(loader) // Load the resources given to the loader
-    .then(() => {    // Once they are loaded...
+  .then(() => {    // Once they are loaded...
+    
+    // Add the map to the scene
     const tm = map.getTileMap(); // Get the generated Excalibur TileMap
     game.add(tm);    // Add the TileMap to the default root scene.
-
-    const actor = new Actor(16, 16, 32, 32, Color.Red);
-    // actor.addDrawing(texture);
-    actor.anchor.setTo(0, 0);
-    game.add(actor);
+    
+    // Add an actor to the scene
+    const actor = new Actor(16, 16, 32, 32, Color.Red); // Add an actor that looks like a red square by default.
+    actor.addDrawing(texture); // Give the actor an actual sprite image.
+    actor.anchor.setTo(0, 0); // Change the actor's center point to be top-left (default is center).
+    game.add(actor); // Add the actor to the map.
   });
 }
